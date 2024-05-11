8a328c6ab529043521b07d561994a4b65dc11413
Thomas Wulz <s1045183@stud.sbg.ac.at>
2024-05-11 11:23:36 +0200

    consistent indentation

--- asm-inc-one.s
+++ asm-inc-one.s
@@ -3,27 +3,27 @@
 
 # entry point
 _start:
-	# load counter address
-	# note: this expands to two instructions!
+    # load counter address
+    # note: this expands to two instructions!
     la a1, counter
-	
-	# critical section start
-	# load current counter value
-	ld a0, 0(a1)
-	# increment register by one
-	addi a0, a0, 1
-	# store incremented value
-	sd a0, 0(a1)
-	# critical section end
-	# implicit synchronization point here, enforced by stutter logic
-	
-	# load counter again, since other thread could have changed it
-	ld a0, 0(a1)
-	
-	# exit ecall with counter as exit code
-	li a7, 93
+
+    # critical section start
+    # load current counter value
+    ld a0, 0(a1)
+    # increment register by one
+    addi a0, a0, 1
+    # store incremented value
+    sd a0, 0(a1)
+    # critical section end
+    # implicit synchronization point here, enforced by stutter logic
+
+    # load counter again, since other thread could have changed it
+    ld a0, 0(a1)
+
+    # exit ecall with counter as exit code
+    li a7, 93
     ecall
-	
+
 
 # shared data segment containing the value to increment
 .data
